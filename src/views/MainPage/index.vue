d26b3fceccdeca3a9f43e52009fbc1863e9ca83c
baozhangjie <baozhangjie@baidu.com>
2025-01-07 20:37:38 +0800

    feat: add main-page data.json

---
+++ src/views/MainPage/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import {ref} from 'vue'
+import data from './data.json'
+
+const state = ref(data)
+</script>
+
+<template>
+    <main>
+        MainPage
+    </main>
+</template>
