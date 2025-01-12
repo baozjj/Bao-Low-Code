c7e61768cb22d5da7d9e4ecdccd084076f3f70aa
baozj <1670370148@qq.com>
2025-01-12 23:39:50 +0800

    feat: add editor css

--- src/views/MainPage/components/Editor/index.vue
+++ src/views/MainPage/components/Editor/index.vue
@@ -7,7 +7,70 @@ withDefaults(defineProps<IEditorProps>(), {
 </script>
 
 <template>
-    <div>
-        {{data}}
+    <div class="editor">
+      <div class="editor-left">左侧物料区</div>
+      <div class="editor-top">菜单栏</div>
+      <div class="editor-right">属性控制栏</div>
+      <div class="editor-container">
+        <!-- 负责产生滚动条 -->
+        <div class="editor-container-canvas">
+          <!-- 产生内容区 -->
+           <div class="editor-container-canvas-content">
+              内容区
+           </div>
+        </div>
+      </div>
+
     </div>
 </template>
+<style lang="less" scoped>
+.editor {
+  width: 100%;
+  height: 100%;
+  
+  .editor-left,
+  .editor-right {
+    position: absolute;
+    width: 270px;
+    background-color: red;
+    top: 0;
+    bottom: 0;
+  }
+
+  .editor-left {
+    left: 0;
+  }
+
+  .editor-right {
+    right: 0;
+  }
+
+  .editor-top {
+    position: absolute;
+    right: 280px;
+    left: 280px;
+    height: 80px;
+    background-color: blue;
+  }
+
+  .editor-container {
+    padding: 80px 270px;
+    height: 100%;
+    box-sizing: border-box;
+      .editor-container-canvas {
+        overflow: scroll;
+        height: 100%;
+
+        .editor-container-canvas-content {
+          margin: 20px auto;
+          width: 1000px;
+          height: 1000px;
+          background-color: green;
+        }
+
+      }
+  }
+
+}
+
+</style>
