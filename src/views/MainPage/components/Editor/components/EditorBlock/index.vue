7e1eef92c3b446dd0191563d4eda78a9805d127e
baozhangjie <baozhangjie@baidu.com>
2025-01-13 23:08:14 +0800

    feat: add EditorBlock

---
+++ src/views/MainPage/components/Editor/components/EditorBlock/index.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { withDefaults, defineProps, computed } from "vue";
+
+const props = withDefaults(defineProps<{
+    block: Object
+}>(), {
+    block: null
+});
+
+const blockStyles = computed(() => {
+    return {
+        top: `${props.block.top}px`,
+        left: `${props.block.left}px`,
+        zIndex: `${props.block.zIndex}px`,
+    }
+})
+</script>
+
+<template>
+    <div class="editor-block" :style="blockStyles">
+        {{block}}
+    </div>
+</template>
+
+<style scoped lang="less">
+.editor-block {
+    position: absolute;
+}
+</style>
